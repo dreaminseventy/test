eb01f7e82337aa92ce7eab29ab39932de3f13cb2
dreaminseventy <yang15983210406@163.com>
2023-07-25 10:08:43 +0800

    添加安全页面

--- src/main/java/com/example/helloworld/controller.java
+++ src/main/java/com/example/helloworld/controller.java
@@ -1,9 +1,11 @@
 package com.example.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/test")
 public class controller {
     @GetMapping("/hello")
     public String hello(){
